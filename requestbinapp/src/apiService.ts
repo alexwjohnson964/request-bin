cd8e0f4bf8e47e629545c71e92348a006506675c
Alex Johnson <alexwjohnson964@gmail.com>
2025-05-14 15:20:57 -0400

    adding api service functions and route handler for retrieving all baskets

--- requestbinapp/src/apiService.ts
+++ requestbinapp/src/apiService.ts
@@ -9,7 +9,23 @@ const axiosConfig = {
   }
 };
 
-export async function getSomething(path: string){
-  const response = await axios.get(`${baseURL}/${path}`);
+
+
+//Post a new basket request 
+export async function newBasket(){
+  const response = await axios.post(`${baseURL}/baskets/new`, axiosConfig);
+  return response.data;
+}
+
+//Get basket 
+  //Get all requests from a basket
+export async function getBasket(basketUrl: string){
+    const response = await axios.get(`${baseURL}/baskets/${basketUrl}`);
+    return response.data;
+}
+  
+//Get all baskets
+export async function getAllBaskets() {
+  const response = await axios.get(`${baseURL}/baskets/all`);
   return response.data;
 }
